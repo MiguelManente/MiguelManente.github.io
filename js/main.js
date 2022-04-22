593aeaf6ed98086ccc89b3095a22c86059f786e5
miguelmanente <102184423+miguelmanente@users.noreply.github.com>
2022-04-22 14:34:33 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded',()=>{
     const elementosCarousel = document.querySelectorAll('.carousel');
     M.Carousel.init(elementosCarousel,{
         duration: 1000,
-        dist: -30,
+        dist: -80,
         shift: 5,
         padding: 5,
         numVisible: 3,
